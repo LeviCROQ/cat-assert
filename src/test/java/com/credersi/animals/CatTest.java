2c8323a24c83fbc979473a47e4cc835a56e245f0
Levi Cole <levi.cole@roq.local>
2023-02-21 14:50:50 +0000

    Simple assert tests implemented to check functionality of cat class

--- src/test/java/com/credersi/animals/CatTest.java
+++ src/test/java/com/credersi/animals/CatTest.java
@@ -12,4 +12,118 @@ public class CatTest {
 		assertTrue(cat.isAlive());
 		assertEquals(cat.howHungry(), 50);
 	}
+	
+	@Test
+	public void testFeedCat() {
+		Cat cat = new Cat();
+		assertTrue(cat.isAlive());
+		cat.feeds();
+		assertEquals(cat.howHungry(), 30);
+	}
+	
+	@Test
+	public void testFeedCatTwo() {
+		Cat cat = new Cat();
+		assertTrue(cat.isAlive());
+		cat.feeds();
+		cat.feeds();
+		assertEquals(cat.howHungry(), 10);
+	}
+	
+	@Test
+	public void testFeedCatThree() {
+		Cat cat = new Cat();
+		assertTrue(cat.isAlive());
+		cat.feeds();
+		cat.feeds();
+		cat.feeds();
+		assertEquals(cat.howHungry(), 0);
+	}
+	
+	@Test
+	public void testFeedCatFour() {
+		Cat cat = new Cat();
+		assertTrue(cat.isAlive());
+		cat.feeds();
+		cat.feeds();
+		cat.feeds();
+		cat.feeds();
+		assertEquals(cat.howHungry(), 0);
+	}
+	
+	@Test
+	public void testCatRun() {
+		Cat cat = new Cat();
+		assertTrue(cat.isAlive());
+		cat.runs();
+		assertEquals(cat.howHungry(), 80);
+	}
+	
+	@Test
+	public void testStarveCat() {
+		Cat cat = new Cat();
+		assertTrue(cat.isAlive());
+		cat.runs();
+		cat.runs();
+		assertEquals(cat.isAlive(), false);
+	}
+	
+	@Test
+	public void testStarveCatTwo() {
+		Cat cat = new Cat();
+		assertTrue(cat.isAlive());
+		cat.feeds();		
+		cat.runs();
+		cat.runs();
+		cat.runs();
+		assertEquals(cat.isAlive(), false);
+	}
+	
+	@Test
+	public void testAlmostStarvingCat() {
+		Cat cat = new Cat();
+		assertTrue(cat.isAlive());
+		cat.feeds();		
+		cat.runs();
+		cat.runs();
+		cat.feeds();
+		cat.feeds();		
+		cat.runs();
+		assertEquals(cat.isAlive(), true);
+	}
+	
+	@Test
+	public void testAlmostStarvingCatTwo() {
+		Cat cat = new Cat();
+		assertTrue(cat.isAlive());
+		cat.runs();
+		cat.feeds();
+		cat.feeds();
+		cat.feeds();
+		cat.runs();
+		cat.runs();
+		assertEquals(cat.isAlive(), true);
+	}
+	
+	@Test
+	public void testOverfeedCat() {
+		Cat cat = new Cat();
+		assertTrue(cat.isAlive());
+		cat.feeds();
+		cat.feeds();
+		cat.feeds();
+		assertEquals(cat.howHungry(), 0);
+	}
+	
+	@Test
+	public void testOverfeedCatTwo() {
+		Cat cat = new Cat();
+		assertTrue(cat.isAlive());
+		cat.feeds();
+		cat.feeds();
+		cat.feeds();
+		cat.feeds();
+		cat.feeds();
+		assertEquals(cat.howHungry(), 0);
+	}
 }
